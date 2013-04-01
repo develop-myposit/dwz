7ad02fd6105cf7098921d023d74ae2e199b02305
Paul Hahn <paulghahn2@gmail.com>
2013-04-01 09:48:51 -0700

    First Application Commit after chapter 10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learn30::Application.config.secret_token = '0d9c4cdb0e9e15b3dad3dfa093cc9588f588c0f6e626a85fefe4a108c808cfb196110aa1cc45b40f6b0df26f1101d1125157be4d3031f98700de20ea44278358'
