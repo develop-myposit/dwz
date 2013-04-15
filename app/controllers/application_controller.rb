ae1d8a2ee6ed6708b77cbcbf3d61f5b2ac33be5c
Paul Hahn <paulghahn2@gmail.com>
2013-04-14 17:13:55 -0700

    Completed Chapter 13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+ before_filter :get_pages_for_tabs
+  
+  def get_pages_for_tabs
+    if user_signed_in?
+      @tabs = Page.all
+    else
+      @tabs = Page.where(:admin => FALSE)
+    end
+  end
+
+  def get_page_metadata
+    @page = Page.find_by_name(params[:name])
+    @pagetitle = @page.title
+  end
 end
