5eeaa92e72621e74abd563a401d09c64e8ed5bd3
Paul Hahn <paulghahn2@gmail.com>
2013-04-15 13:40:26 -0700

    Added markdown support using Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+   def markdown(text)
+   	options ={ :hard_wrap => true,
+   	           :autolink => true,
+   	           :no_intra_emphasis => true,
+   	           :filter_html => true }
+      dmarkdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+      dmarkdown.render(text).html_safe
+   end
 end
